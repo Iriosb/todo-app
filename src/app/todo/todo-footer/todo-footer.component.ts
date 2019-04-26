bf52f6c2466b903f1179fe2cac47b4092d522ba1
Isis Rios <isisrios47e@gmail.com>
2019-04-26 15:53:07 -0500

    Todo App Redux Finished

---
+++ src/app/todo/todo-footer/todo-footer.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import * as fromFiltro from './../../filter/filter.actions';
+import * as fromTodo from './../../todo/todo.actions';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducers';
+import { Todo } from '../model/todo.model';
+
+@Component({
+  selector: 'app-todo-footer',
+  templateUrl: './todo-footer.component.html',
+  styles: []
+})
+export class TodoFooterComponent implements OnInit {
+
+  filtrosValidos: fromFiltro.filtrosValidos[] = ['todos', 'completados', 'pendientes'];
+  filtroActual: fromFiltro.filtrosValidos;
+  pendientes: number;
+
+  constructor(private store: Store<AppState>) { }
+
+  ngOnInit() {
+    this.store.subscribe(state => {
+      this.contarPendientes(state.todos);
+      this.filtroActual = state.filtro;
+    })
+  }
+
+  cambiarFiltro(nuevoFiltro: fromFiltro.filtrosValidos){
+    this.store.dispatch(new fromFiltro.SetFiltroAction(nuevoFiltro));
+  }
+
+  contarPendientes(todos: Todo[]) {
+    this.pendientes = todos.filter( todo => !todo.completado).length;
+  }
+
+  borrarTodo() {
+    this.store.dispatch(new fromTodo.BorrarAllTodoAction());
+  }
+
+}
